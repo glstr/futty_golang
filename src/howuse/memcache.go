1dc2e778ce47ba3f18cfe03b1070826fccff6349
glstr <glstrsc@163.com>
2018-10-12 16:33:38 +0800

    add memcache

--- src/howuse/memcache.go
+++ src/howuse/memcache.go
@@ -1,1 +1,112 @@
 package howuse
+
+import (
+	"errors"
+	"sync"
+	"time"
+)
+
+var (
+	dfInterval = int64(5)
+	dfSize     = int64(10)
+	dfExpire   = int64(5) //unit s
+)
+
+// memcache basic version
+type cache map[string]obExpire
+
+type obExpire struct {
+	createTime int64
+	d          interface{}
+}
+
+type MemCache struct {
+	c        cache
+	mu       sync.RWMutex
+	interval int64
+	expire   int64
+	done     chan struct{}
+}
+
+func NewMemCache() *MemCache {
+	m := &MemCache{
+		c:        make(map[string]obExpire, dfSize),
+		interval: dfInterval,
+		expire:   dfExpire,
+		done:     make(chan struct{}),
+	}
+	go m.clean()
+	return m
+}
+
+func (m *MemCache) Add(key string, ele interface{}) error {
+	defer m.mu.Unlock()
+	m.mu.Lock()
+	if _, ok := m.c[key]; ok {
+		return errors.New("key exist")
+	}
+	v := obExpire{
+		createTime: time.Now().Unix(),
+		d:          ele,
+	}
+	m.c[key] = v
+	return nil
+}
+
+func (m *MemCache) Delete(key string) bool {
+	defer m.mu.Unlock()
+	m.mu.Lock()
+	delete(m.c, key)
+	return true
+}
+
+func (m *MemCache) Get(key string) (interface{}, error) {
+	defer m.mu.Unlock()
+	m.mu.Lock()
+	v, ok := m.c[key]
+	if !ok {
+		return nil, errors.New("key not exist")
+	} else {
+		return v.d, nil
+	}
+}
+
+func (m *MemCache) Update(key string, ele interface{}) bool {
+	v := obExpire{
+		createTime: time.Now().Unix(),
+		d:          ele,
+	}
+
+	defer m.mu.Unlock()
+	m.mu.Lock()
+	m.c[key] = v
+	return true
+}
+
+func (m *MemCache) Close() {
+	m.done <- struct{}{}
+}
+
+func (m *MemCache) clean() {
+	for {
+		select {
+		case <-m.done:
+			m.done <- struct{}{}
+			break
+		default:
+			m.cl()
+			time.Sleep(time.Duration(m.interval) * time.Second)
+		}
+	}
+}
+
+func (m *MemCache) cl() {
+	defer m.mu.Unlock()
+	m.mu.Lock()
+	t := time.Now().Unix()
+	for k, v := range m.c {
+		if t-v.createTime > m.expire {
+			delete(m.c, k)
+		}
+	}
+}
---
+++ src/howuse/memcache_test.go
@@ -0,0 +1,64 @@
+package howuse
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+var m *MemCache
+
+func makeEnv() {
+	m = NewMemCache()
+	m.Add("hello", 1)
+	m.Add("world", 2)
+	m.Add("gogogo", 3)
+	m.Add("snow", 4)
+}
+
+type FuncGet func(m *MemCache, key string, t *testing.T)
+type FuncUpdate func(m *MemCache, key string, value int, t *testing.T)
+
+func Test_MemCache(t *testing.T) {
+	makeEnv()
+	var keys = []string{"hello", "world", "gogogo", "snow"}
+
+	getFunc := func(m *MemCache, key string, t *testing.T) {
+		ret, _ := m.Get(key)
+		//t.Logf("res:%v", ret)
+		fmt.Printf("res:%v\n", ret)
+	}
+
+	updateFunc := func(m *MemCache, key string, value int, t *testing.T) {
+		ret := m.Update(key, value)
+		//t.Logf("res:%v", ret)
+		fmt.Printf("res:%v\n", ret)
+	}
+
+	getWorker := func(f FuncGet, m *MemCache, key string, t *testing.T) {
+		for {
+			f(m, key, t)
+			time.Sleep(1 * time.Second)
+		}
+	}
+
+	updateWorker := func(f FuncUpdate, m *MemCache, key string, value int, t *testing.T) {
+		for {
+			f(m, key, value, t)
+			time.Sleep(1 * time.Second)
+		}
+	}
+
+	for i := 0; i < 10; i++ {
+		index := i % 3
+		go getWorker(getFunc, m, keys[index], t)
+	}
+
+	for i := 0; i < 10; i++ {
+		index := i % 3
+		go updateWorker(updateFunc, m, keys[index], i, t)
+	}
+
+	c := make(chan struct{})
+	<-c
+}
