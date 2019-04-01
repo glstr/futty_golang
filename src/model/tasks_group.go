aa92f7a4e0210c9a7329d8065c42e952c15a540b
glstr <glstrsc@163.com>
2019-04-01 23:24:57 +0800

    add task group task

--- src/model/tasks_group.go
+++ src/model/tasks_group.go
@@ -1,8 +1,11 @@
 package model
 
+import "fmt"
+
 type TaskWorker struct {
-	workList chan work
-	done     chan struct{}
+	workList    chan work
+	done        chan struct{}
+	workDoneNum int64
 }
 
 func NewTaskWorker(wl chan work, d chan struct{}) *TaskWorker {
@@ -19,6 +22,7 @@ func (tw *TaskWorker) Work() {
 			return
 		case work := <-tw.workList:
 			work()
+			tw.workDoneNum++
 		}
 	}
 }
@@ -26,6 +30,7 @@ func (tw *TaskWorker) Work() {
 type TasksGroup struct {
 	workers        []*TaskWorker
 	workList       chan work
+	done           chan struct{}
 	workListLength int64
 	workersNum     int64
 }
@@ -33,17 +38,41 @@ type TasksGroup struct {
 type work func()
 
 func NewTasksGroup(wll, wn int64) *TasksGroup {
-	return &TasksGroup{}
+	return &TasksGroup{
+		workListLength: wll,
+		workersNum:     wn,
+		done:           make(chan struct{}),
+		workList:       make(chan work, wn),
+	}
 }
 
+//Add provides operation to add work to worklist
 func (t *TasksGroup) Add(w work) {
+	t.workList <- w
 }
 
+//Cancel provides cancel operation for task group
 func (t *TasksGroup) Cancel() {
+	close(t.done)
 }
 
+//Stats provides stat info
 func (t *TasksGroup) Stats() {
+	for index, worker := range t.workers {
+		fmt.Printf("index:%d, workDone:%d\n", index, worker.workDoneNum)
+	}
 }
 
+//Start provides start task group loop
 func (t *TasksGroup) Start() {
+	//init workers
+	for i := int64(0); i < t.workersNum; i++ {
+		w := NewTaskWorker(t.workList, t.done)
+		t.workers = append(t.workers, w)
+	}
+
+	//start loop
+	for _, work := range t.workers {
+		go work.Work()
+	}
 }
---
+++ src/model/tasks_group_test.go
@@ -0,0 +1,30 @@
+package model
+
+import (
+	"testing"
+	"time"
+)
+
+func TestTaskGroup(t *testing.T) {
+	//init random work
+	work := func() {
+		//res := rand.Int63()
+	}
+	wg := NewTasksGroup(10, 100)
+	go func() {
+		for i := 0; i < 10000; i++ {
+			wg.Add(work)
+		}
+	}()
+
+	wg.Start()
+
+	go func() {
+		for {
+			wg.Stats()
+			time.Sleep(2 * time.Second)
+		}
+	}()
+	done := make(chan struct{})
+	<-done
+}
